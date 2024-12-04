7d77983d4420ad5d8c0dda11580f100606729813
tristanperich <mail@tristanperich.com>
2024-12-03 23:13:38 -0500

    Add instrument example

---
+++ Examples/12-instrument.asm
@@ -0,0 +1,43 @@
+; Include chip definition
+.include "tn85def.inc"
+
+; Set data-direction of pin B4 to output
+sbi DDRB, 4
+
+; Turn on pull-up resistor for pin B0
+sbi PORTB, 0
+
+; Loop forever...
+Loop:
+	; Skip if the button is not pressed
+	sbis PINB, 0
+		; Decrement the duration
+		dec r18
+	
+	; Turn on pin B4
+	sbi PORTB, 4
+	
+	; Wait (copy duration into register and call function)
+	mov r16, r18
+	rcall Wait
+	
+	; Turn off pin B4
+	cbi PORTB, 4
+	
+	; Wait (copy duration into register and call function)
+	mov r16, r18
+	rcall Wait
+	
+	; Jump to Loop
+	rjmp Loop
+
+; Loop until done waiting...
+Wait:
+	
+	; Decrement the register
+	dec r16
+	
+	; If result does not equal 0, branch back to the Wait loop
+	brne Wait
+; Return from function
+ret
